10837f34bc2bf3131aafaf6693347e95883f5c50
JIN <a1824jj@aiit.ac.jp>
2020-05-14 01:59:22 +0000

    get ActivatedElement

--- DailyFoodScore/food-app.js
+++ DailyFoodScore/food-app.js
@@ -7,19 +7,36 @@ var Score = /** @class */ (function () {
 var Food = /** @class */ (function () {
     function Food(element) {
         this.element = element;
-        element.addEventListener('click', this.clickEventHandler);
+        element.addEventListener('click', this.clickEventHandler.bind(this));
     }
     Food.prototype.clickEventHandler = function () {
+        console.log(this);
+        this.element.classList.toggle('food--active');
     };
     return Food;
 }());
 var Foods = /** @class */ (function () {
     function Foods() {
         this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
         this.elements.forEach(function (element) {
             new Food(element);
         });
     }
+    Object.defineProperty(Foods.prototype, "activeElements", {
+        get: function () {
+            var _this = this;
+            this._activeElements = [];
+            this.elements.forEach(function (element) {
+                if (element.classList.contains('food--active')) {
+                    _this._activeElements.push(element);
+                }
+            });
+            return this._activeElements;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Foods;
 }());
 var foods = new Foods();
