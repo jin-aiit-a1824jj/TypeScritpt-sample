3a99128e3c7fcc33037daa351922bfc300ec3484
JIN <a1824jj@aiit.ac.jp>
2020-05-14 08:01:29 +0000

    ts module node module using sample

---
+++ NodejsExpressSample/src/practice/practice-export.ts
@@ -0,0 +1,7 @@
+export default 'hello'
+export const name = 'Peter';
+
+// const person = {
+//   name: 'Peter'
+// }
+// export = person
---
+++ NodejsExpressSample/src/practice/practice-import.ts
@@ -0,0 +1,4 @@
+//import practice from './practice-export';
+//import { default as practice } from './practice-export';
+import practice = require('./practice-export');
+console.log(practice);
