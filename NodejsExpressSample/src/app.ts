47581f93b5dbe20863f39bc664d5b175422a023c
JIN <a1824jj@aiit.ac.jp>
2020-05-14 08:31:25 +0000

    express sample & add

--- NodejsExpressSample/src/app.ts
+++ NodejsExpressSample/src/app.ts
@@ -1,13 +1,23 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  }
+}
 const app = express();
-app.get((req, res, next) => {
+app.use(express.json());
+app.use((req, res, next) => {
   console.log('hello!');
   next();
 })
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello</h1>');
 })
-app.get((err,req, res, next) => {
+app.post('/', (req: MessageRequest, res, next) => {
+  console.log(req.body);
+  res.send(`<h1>I got ${req.body.message}</h1>`);
+})
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   next();
 })
 app.listen(3000);
