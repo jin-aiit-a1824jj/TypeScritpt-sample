44aa4ddb8295d07896134cdf4b9dad23298ac269
JIN <a1824jj@aiit.ac.jp>
2020-05-14 07:34:52 +0000

    check reach hooks, redux, routes

--- sampel-ts-react-app/src/App.tsx
+++ sampel-ts-react-app/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
 
 function App() {
+  const [name, setName] = useState<string | null>('Peter');
   return (
-    <Hello message="I am Peter"></Hello>
+    <Hello message={`I am ${name}`}></Hello>
   );
 }
 
