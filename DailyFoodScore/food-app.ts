68322a439b383ea147227b41b096d822ebad7bf7
JIN <a1824jj@aiit.ac.jp>
2020-05-14 02:03:09 +0000

    get activatedElement score

--- DailyFoodScore/food-app.ts
+++ DailyFoodScore/food-app.ts
@@ -24,6 +24,18 @@ class Foods {
     return this._activeElements;
   }
 
+
+  private _activeElementsScore: number[] = [];
+  get activeElementsScore(){
+    this._activeElementsScore = [];
+    this.activeElements.forEach(element => {
+      const foodScore = element.querySelector('.food__score');
+      if (foodScore){
+        this._activeElementsScore.push(Number(foodScore.textContent));
+      }
+    })
+    return this._activeElementsScore;
+  }
   constructor() {
     this.elements.forEach(element =>{
       new Food(element);
