b593bb68c797159b409960ddb807103af90fc5af
JIN <a1824jj@aiit.ac.jp>
2020-05-14 07:31:24 +0000

    usig props & add property

--- sampel-ts-react-app/src/App.tsx
+++ sampel-ts-react-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello'
 
 function App() {
   return (
-    <div>Hello</div>
+    <Hello message="I am Peter"></Hello>
   );
 }
 
