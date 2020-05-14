a289526636a483e97bd5d33c1146eacc89491966
JIN <a1824jj@aiit.ac.jp>
2020-05-14 08:21:40 +0000

    sample express

--- NodejsExpressSample/src/app.ts
+++ NodejsExpressSample/src/app.ts
@@ -1,4 +1,13 @@
-//import * as express from 'express';
-import express = require("express");
+import express from 'express';
 const app = express();
+app.get((req, res, next) => {
+  console.log('hello!');
+  next();
+})
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello</h1>');
+})
+app.get((err,req, res, next) => {
+  next();
+})
 app.listen(3000);
