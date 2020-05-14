a539bedaf8b2f0e5b66802dc44c4d92dacf58329
JIN <a1824jj@aiit.ac.jp>
2020-05-14 02:56:12 +0000

    disassembly - food-app.ts

---
+++ WebPackSample/src/foods.ts
@@ -0,0 +1,39 @@
+class Foods implements Foodsable{
+
+  elements = document.querySelectorAll<HTMLDivElement>('.food');
+  private _activeElements: HTMLDivElement[] = [];
+  get activeElements(){
+    this._activeElements = [];
+    this.elements.forEach(element => {
+      if (element.classList.contains('food--active')){
+        this._activeElements.push(element);
+      }
+    })
+    return this._activeElements;
+  }
+
+
+  private _activeElementsScore: number[] = [];
+  get activeElementsScore(){
+    this._activeElementsScore = [];
+    this.activeElements.forEach(element => {
+      const foodScore = element.querySelector('.food__score');
+      if (foodScore){
+        this._activeElementsScore.push(Number(foodScore.textContent));
+      }
+    })
+    return this._activeElementsScore;
+  }
+  private constructor() {
+    this.elements.forEach(element =>{
+      new Food(element);
+    })
+  }
+
+  private static instance:Foods;
+  static getInstance(){
+    if (!Foods.instance)
+      Foods.instance = new Foods();
+    return Foods.instance;
+  }
+}
---
+++ WebPackSample/src/score.ts
@@ -0,0 +1,17 @@
+class Score implements Scoreable{
+  get totalScore(){
+    const foods = Foods.getInstance();
+    return foods.activeElementsScore.reduce((total, score) => total + score, 0);
+  }
+  render(){
+    document.querySelector('.score__number')!.textContent = String(this.totalScore);
+  }
+
+  private static instance:Score;
+  private constructor(){}
+  static getInstance(){
+    if (!Score.instance)
+    Score.instance = new Score();
+    return Score.instance;
+  }
+}
